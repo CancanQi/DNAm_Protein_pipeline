e89f064ce392f58ee8932e97e3b9b602f4e0ea3a
CancanQi <48548477+CancanQi@users.noreply.github.com>
2025-11-03 17:01:07 +0800

    Update Job.submit.sh

--- 1.EWAS/Job.submit.sh
+++ 1.EWAS/Job.submit.sh
@@ -6,12 +6,12 @@
 #SBATCH -o R_%A_%a.out # Standard output
 #SBATCH -e R_%A_%a.err # Standard error
 
-source /share/opt/anaconda3/bin/activate r_env
+source /share/opt/anaconda3/bin/activate r_env # activate the environment
 
-cd /share/users/qicancan/DNAmethylation/demo
+cd /share/users/qicancan/DNAmethylation/demo # change to working directory
 
 Rscript EWAS_demo_script.R \
-        --input DNAm.meta.demo.csv \
-        --output results.demo.txt \
-        --cols CRF,Age,Gender,BMI \
+        --input DNAm.meta.demo.csv \ # input meta data
+        --output results.demo.txt \ # name of output file
+        --cols CRF,Age,Gender,BMI \ # colunms invovled in the analysis, the first one is the phenotype you are interested
         --verbose
